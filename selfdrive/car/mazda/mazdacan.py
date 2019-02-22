fd584d64fbc113b3c8dfe151a92102f10ad2cccd
Jafar Al-Gharaibeh <to.jafar@gmail.com>
2019-02-21 23:15:07 -0600

    Enable tx hook
    Signed-off-by: Jafar Al-Gharaibeh <to.jafar@gmail.com>

--- selfdrive/car/mazda/mazdacan.py
+++ selfdrive/car/mazda/mazdacan.py
@@ -11,4 +11,4 @@ def create_steering_control(packer, bus, car_fingerprint, idx, steer, checksum):
       "CHKSUM": checksum
     }
 
-  return packer.make_can_msg("CAM_LKAS", 0, values)
+  return packer.make_can_msg("CAM_LKAS", bus, values)
